1e8b0ca2530928908034d774792d84fc97fed61a
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-01-06 14:13:11 +0530

    Update main.py
    Added more functionalities

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import random
+import re
+import secrets
 import string
-import secrets  # Add this import for secrets module
 
-# Define the possible characters for the password
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
 
-# Combine all characters
-all_characters = letters + digits + symbols
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
 
-print(all_characters)
+    # Combine all characters
+    all_characters = letters + digits + symbols
 
-def generate_password(length):
-    password = ''
-    # Generate password
-    for _ in range(length):
-        password += secrets.choice(all_characters)
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        
+        constraints = [
+            (nums, '')
+        ]        
+
     return password
